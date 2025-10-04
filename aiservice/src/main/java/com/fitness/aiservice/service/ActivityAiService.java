8d70202a9115aab946eecb764ece0fa97ca2a063
Aaditya Mohan <91790579+AadityaMohan-dev@users.noreply.github.com>
2025-10-04 12:46:23 +0530

    improvents in ai-service

--- aiservice/src/main/java/com/fitness/aiservice/service/ActivityAiService.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityAiService.java
@@ -1,20 +1,84 @@
 package com.fitness.aiservice.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fitness.aiservice.model.Activity;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class ActivityAiService {
+
     private final GeminiService geminiService;
+
     public String generateRecommendation(Activity activity){
         String prompt = createPromptForActivity(activity);
         String aiResponse = geminiService.getAnswer(prompt);
         log.info("RESPONSE FROM AI: {}", aiResponse);
+        return processAiResponse(activity , aiResponse);
+    }
+
+    private String processAiResponse(Activity activity, String aiResponse){
+        try{
+            ObjectMapper mapper =new ObjectMapper();
+            JsonNode rootNode = mapper.readTree(aiResponse);
+
+            JsonNode textNode = rootNode.path("candidates")
+                    .get(0)
+                    .path("content")
+                    .path("parts")
+                    .get(0)
+                    .path("text");
+
+            String jsonContent = textNode.asText()
+                    .replaceAll("```json\\n", "")
+                    .replaceAll("\n```", "")
+                    .trim();
+
+            JsonNode analysisJson = mapper.readTree(jsonContent);
+            JsonNode analysisNode = analysisJson.path("analysis");
+            StringBuilder fullAnalysis = new StringBuilder();
+
+            addAnalysisSection(fullAnalysis , analysisNode , "overall", "Overall :");
+            addAnalysisSection(fullAnalysis , analysisNode , "pace", "Pace :");
+            addAnalysisSection(fullAnalysis , analysisNode , "heartRate", "Heart Rate :");
+            addAnalysisSection(fullAnalysis , analysisNode , "caloriesBurned", "Calories :");
+
+            List<String> improvements = extractImprovements(analysisJson.path("improvements"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return aiResponse;
+    }
+
+    private List<String> extractImprovements(JsonNode improvementsNode) {
+        List<String> improvements = new ArrayList<>();
+        if(improvementsNode.isArray()){
+            improvementsNode.forEach(improvement -> {
+                String area = improvement.path("area").asText();
+                String detail = improvement.path("Recommendation").asText();
+
+                improvements.add(String.format("%s: %s", area, detail));
+            });
+
+            return improvements.isEmpty() ? Collections.singletonList("No Specific improvements provided") : improvements;
+        }
+        return improvements;
+    }
+
+    private void addAnalysisSection(StringBuilder fullAnalysis, JsonNode analysisNode, String key, String prefix) {
+        if(!analysisNode.path(key).isMissingNode()){
+            fullAnalysis.append(prefix)
+                    .append(analysisNode.path(key).asText())
+                    .append("\n\n");
+        }
     }
 
     private String createPromptForActivity(Activity activity) {
--- aiservice/src/main/java/com/fitness/aiservice/service/activityMessageListener.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/activityMessageListener.java
@@ -4,7 +4,6 @@ import com.fitness.aiservice.model.Activity;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
